b5d4b68ab6847d1558050a069b0a5f3e2e0f2ea1
setter-getter <dimkinyashchik@yandex.ru>
2023-01-15 11:24:40 +0400

    refactor: change imdex.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverseq(n) {
+    if (n < 0) {
+        n = n * (-1)
+    }
+    let arr = String(n).split('');
+    if (arr[arr.length - 1] == '0') {
+        arr = arr.slice(0, arr.length - 1)
+    }
+    return arr.reverse().join('');
 }
